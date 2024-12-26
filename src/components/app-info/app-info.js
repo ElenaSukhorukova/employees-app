331fda889abff558e316bc3c026c2fdaf10ea3e0
Elena Suchorukova <suhorukova.el@gmail.com>
2024-12-26 13:51:40 +0400

    Add some logic

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import '.app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Accounting of employees in the company Ltd. "Dream Job"</h1>
+      <h2>Final count of employees</h2>
+      <h2>Benefit receives: </h2>
+    </div>
+  );
+}
+
+export default AppInfo;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// const text = 'Hello world';
-
-// const elem = (
-//   <div>
-//       <h2 className="class-name">{text}</h2>
-//       <lable htmlFor="for-input">Lable for input</lable>
-//       <input type="text" />
-//       <button />
-//   </div>
-// );
-
 root.render(
-  // <StrictMode>
+  <React.StrictMode>
     <App />
-  // </StrictMode>
+  </React.StrictMode>
 );
