693d3698d853a78ec08070bff4863dcf484c67e3
Elena Suchorukova <suhorukova.el@gmail.com>
2024-12-28 11:14:58 +0400

    Add some logic

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,25 +1,50 @@
 import './employees-add-form.css'
+import {Component} from 'react'
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add a new employee</h3>
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      slalary: ''
+    }
+  }
 
-      <form action="POST"
-            className="add-form d-flex">
-          <input type="text"
-                 className="form-control new-post-lable"
-                 placeholder="How is ther name?"/>
-          <input type="text"
-                 className="form-control new-post-lable"
-                 placeholder="Salary in $?"/>
-          <button type="submit"
-                  className="btn btn-outline-light">
-            Add
-          </button>
-        </form>
-    </div>
-  );
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  render() {
+    const {name, salary} = this.state
+
+    return (
+      <div className="app-add-form">
+        <h3>Add a new employee</h3>
+
+        <form action="POST"
+              className="add-form d-flex">
+            <input type="text"
+                   className="form-control new-post-lable"
+                   placeholder="How is ther name?"
+                   name="name"
+                   value={name}
+                   onChange={this.onValueChange}/>
+            <input type="text"
+                   className="form-control new-post-lable"
+                   placeholder="Salary in $?"
+                   name="salary"
+                   value={salary}
+                   onChange={this.onValueChange}/>
+            <button type="submit"
+                    className="btn btn-outline-light">
+              Add
+            </button>
+          </form>
+      </div>
+    );
+  }
 }
 
 export default EmployeesAddForm;
